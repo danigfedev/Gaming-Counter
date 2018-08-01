8e01e5ccd48b1b9643250f8336391b33784c7628
danigfedev <danigfe.dev@gmail.com>
2018-08-01 19:11:22 +0200

    [Unity Project] Game data entry, display in list view and store in xml format

---
+++ GameHourRegister/Assets/Scripts/VideoGame.cs
@@ -0,0 +1,64 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+public class VideoGame{
+
+    private string Title;
+    private string Platform;
+    private string Price;
+    private string Time;
+
+    public VideoGame()//Default COnstructor
+    {
+
+    }
+
+    public VideoGame(string title, string platform, string price, string time)
+    {
+        Title = title;
+        Platform = platform;
+        Price = price;
+        Time = time;
+    }
+
+    public string GetTitle()
+    {
+        return Title;
+    }
+
+    public void SetTitle(string t)
+    {
+        Title = t;
+    }
+
+    public string GetPlatform()
+    {
+        return Platform;
+    }
+
+    public void SetPlatform(string pl)
+    {
+        Platform = pl;
+    }
+
+    public string GetPrice()
+    {
+        return Price;
+    }
+
+    public void SetPrice(string pr)
+    {
+        Price = pr;
+    }
+
+    public string GetTime()
+    {
+        return Time;
+    }
+    
+    public void SetTime(string time)
+    {
+        Time = time;
+    }
+}
